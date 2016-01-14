da2bd007766bf2147edfbcc4f37e63f5cca56bbc
liquidtrends <ryan@liquidtrends.com>
2016-01-14 11:36:49 -0700

    Added mobile friendly text

--- public/js/main.js
+++ public/js/main.js
@@ -9,20 +9,52 @@ $(document).ready(function() {
     $('#equipment').DataTable({
     	paging: false
     });
-} );
+});
 
-var feedApp = angular.module('feedDataApp', ['firebase']);
- 
-feedApp.controller('feedListController', function ($scope, $firebase) {
-    var fbURL = new Firebase("https://tdn.firebaseio.com/Jobs/");
-    $scope.feedsList = $firebase(fbURL);
- 
-    $scope.save = function () {
-        $scope.feedsList.$add({
-            Title: $scope.feedsList.title,
-            Description: $scope.feedsList.description
-             
-        });
-        $(":text").val('');
-    }
+// Job Listings //
+
+var jobListings = new Firebase('https://tdn.firebaseio.com/Jobs');
+jobListings.on('child_added', function(snapshot){
+    var Jobs = snapshot.val();
+    displayJobs(Jobs.title, Jobs.description)
+
+});
+
+function displayJobs(title, description){
+    $('<div class="well"/>').text(description).prepend($('<div/>').text(title))
+        .appendTo($('#jobsDiv'));
+};
+
+// var deferred = displayJobs();
+
+// $.when(deferred).done(function() {
+//      if($('#jobsDiv').is(':empty')){
+//         $('.no-jobs').show();
+//     } else {
+//         $('.no-jobs').hide();
+//     }
+// });
+
+//Contact Form//
+
+$("#contact-form").validate({
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/liquidtrends@gmail.com", 
+      method: "POST",
+      data: {
+        name: $(form).find("input[name='name']").val(),
+        _replyto: $(form).find("input[name='_replyto']").val(),
+        message: $(form).find("textarea[name='message']").val()
+      },
+      dataType: "json",
+      success: function() {
+        $("#submit-success").fadeIn();
+        $("#contact-form").fadeOut();
+      },
+      error: function() {
+        $("#submit-errors").fadeIn();        
+      }
+    });
+  }
 });
